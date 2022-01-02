f880c023047e1d0436d0c24bfb828eb013f0e1b6
JustinOrji <justinorji2@gmail.com>
2022-01-01 20:57:55 -0600

    Initial Commit

---
+++ LuoLandmarks/LuoLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuoLandmarksApp.swift
+//  LuoLandmarks
+//
+//  Created by Justin Orji on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LuoLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
